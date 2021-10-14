e804fdd4f380fbfc9dbe3f4c96d75e84aac89952
Temych11 <80815377+Temych11@users.noreply.github.com>
2021-10-14 13:17:45 +0700

    Add files via upload

---
+++ JCHW7/DatabaseRepository.java
@@ -0,0 +1,13 @@
+package JCHW7;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+// Репозиторий для работы
+public interface DatabaseRepository {
+
+    boolean saveWeatherData(WeatherResponse weatherResponse) throws SQLException;
+
+    List<WeatherResponse> getAllSavedData() throws IOException;
+}
